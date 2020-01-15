dfb9dba0d1ff45873fb01c9944147a6688e83447
JehyunRa <jra4862@gmail.com>
2020-01-15 17:33:01 +0000

    2

--- client.js
+++ client.js
@@ -8,9 +8,23 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on('connect', () => {
+    console.log('Connected');
+    conn.write('Name: JJR');
+    // for (let i = 1; i < 10; i++) {
+    //   setInterval(() => conn.write("Move: up"), i * 1000);
+    // }
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
 
+  // "Move: up" - move up one square (unless facing down)
+  // "Move: down" - move down one square (unless facing up)
+  // "Move: left" - move left one square (unless facing right)
+  // "Move: right" - move left one square (unless facing left)
+
   return conn;
 }
+
+module.exports = { connect };
