1b2f9a40c23087fb1194617500639a7fe8cbbdca
JehyunRa <jra4862@gmail.com>
2020-01-15 18:06:10 +0000

    4

--- client.js
+++ client.js
@@ -5,16 +5,14 @@ const connect = function() {
     host: '172.46.2.204',
     port: 50541
   });
-  // interpret incoming data as text
+
   conn.setEncoding('utf8'); 
 
   conn.on('connect', () => {
     console.log('Connected');
     conn.write('Name: JJR');
-    // for (let i = 1; i < 10; i++) {
-    //   setInterval(() => conn.write("Move: up"), i * 1000);
-    // }
   });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
